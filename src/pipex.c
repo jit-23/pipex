f29bd0dd84f7d5feedb1e2d5119740db924ef03e
Fernando De Jesus Lucart <fde-jesu@c1r16s7.42porto.com>
2024-01-13 05:30:11 +0000

    done

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: fde-jesu <fde-jesu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 00:21:22 by fde-jesu          #+#    #+#             */
-/*   Updated: 2024/01/13 04:42:06 by fde-jesu         ###   ########.fr       */
+/*   Updated: 2024/01/13 05:28:56 by fde-jesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ void	proccess_child(t_pipex *pp, int *fd, char **env)
 
 void	proccess_child2(t_pipex *pp, int *fd, char **env)
 {
+	waitpid(-1, 0, 0);
 	close(fd[1]);
 	dup2(fd[0], STDIN_FILENO);
 	dup2(pp->fd2, STDOUT_FILENO);
@@ -40,15 +41,9 @@ void	pp(t_pipex *pipex, char **env)
 	id1 = fork();
 	if (id1 == 0)
 		proccess_child(pipex, fd, env);
-	else
-	{
-		id2 = fork();
-		if (id2 == 0)
-		{
-			waitpid(-1, 0, 0);
-			proccess_child2(pipex, fd, env);
-		}
-	}
+	id2 = fork();
+	if (id2 == 0)
+		proccess_child2(pipex, fd, env);
 	waitpid(-1, 0, 0);
 	close(fd[0]);
 	close(fd[1]);
